2bbe42bd7f83c7da0d91c0c7922cf9546353ee2b
bonanyuan <by424@nyu.edu>
2017-05-02 15:55:57 -0400

    change the template

--- superset_config.py
+++ superset_config.py
@@ -3,34 +3,7 @@ import sys
 from superset import config
 import shutil
 import stat
-
-def copytree(src, dst, symlinks = False, ignore = None):
-  if not os.path.exists(dst):
-    os.makedirs(dst)
-    shutil.copystat(src, dst)
-  lst = os.listdir(src)
-  if ignore:
-    excl = ignore(src, lst)
-    lst = [x for x in lst if x not in excl]
-  for item in lst:
-    s = os.path.join(src, item)
-    d = os.path.join(dst, item)
-    if symlinks and os.path.islink(s):
-      if os.path.lexists(d):
-        os.remove(d)
-      os.symlink(os.readlink(s), d)
-      try:
-        st = os.lstat(s)
-        mode = stat.S_IMODE(st.st_mode)
-        os.lchmod(d, mode)
-      except:
-        pass # lchmod not available
-    elif os.path.isdir(s):
-      copytree(s, d, symlinks, ignore)
-    else:
-      shutil.copy2(s, d)
-
-
+from copy_replace_files import copytree
 
 # BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 #---------------------------------------------------------
@@ -55,7 +28,7 @@ SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'  # noqa
 # Superset metadata (slices, connections, tables, dashboards, ...).
 # Note that the connection information to connect to the datasources
 # you want to explore are managed directly in the web UI
-SQLALCHEMY_DATABASE_URI = 'postgresql://bonan:ybn1992615@tudiio.ceo7x9fchbd7.us-east-1.rds.amazonaws.com:5432/superset_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://admin:admin@localhost:5432/superset'
 MAPBOX_API_KEY = 'pk.eyJ1IjoiYm9uYW55IiwiYSI6ImNqMTczeW9zNzA0OWEzOG9kOTc4MDR5NGwifQ.1KjCtkIS4swYYL9ht5qDww'
 # Flask-WTF flag for CSRF
 CSRF_ENABLED = False
@@ -80,7 +53,7 @@ copytree('superset', relative_path)
 print ("files copied")
 
 APP_ICON = "/static/assets/images/data_top.png"
-
+AUTH_ROLE_PUBLIC = 'Public'
 
 
 
